32cc805de09ea1ba25b95123a13497974b501c1d
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-15 21:51:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 
-async function startBot() {
-  const { state, saveCreds } = await useMultiFileAuthState("auth_info");
+const http = require('http');
 
-  const sock = makeWASocket({
-    auth: state,
-    printQRInTerminal: false
-  });
+http.createServer((req, res) => {
+  res.end("Naveed MD Bot Running");
+}).listen(process.env.PORT || 3000);
 
-  sock.ev.on("creds.update", saveCreds);
-
-  sock.ev.on("connection.update", (update) => {
-    const { connection, qr } = update;
-
-    if (connection === "open") {
-      console.log("🤖 Naveed MD Bot Connected");
-    }
-
-    if (qr) {
-      console.log("Scan QR or use Pair Code method in next step");
-    }
-  });
-}
-
-startBot();
+console.log("Bot Started");
