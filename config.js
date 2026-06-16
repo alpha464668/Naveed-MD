39c78ad257d02a08216547790a02b79dee4e89cd
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 20:21:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  BOT_NAME: "Naveed MD",
-  OWNER_NAME: "Naveed",
-  PREFIX: "."
+BOT_NAME: "Naveed MD",
+OWNER_NAME: "Naveed",
+OWNER_NUMBER: "923213231887",
+CHANNEL_LINK: "https://whatsapp.com/channel/0029Vawbj7e5kg7AFF5MuZ28",
+PREFIX: "."
 }
