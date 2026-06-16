54ec44026fc0e8b0b0813bbbe7e767ec69ae89d7
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 17:42:58 +0500

    Create uptime.js

---
+++ uptime.js
@@ -0,0 +1,12 @@
+module.exports = {
+name: "uptime",
+run: async () => {
+const uptime = process.uptime();
+const hours = Math.floor(uptime / 3600);
+const minutes = Math.floor((uptime % 3600) / 60);
+const seconds = Math.floor(uptime % 60);
+
+return `⏱ Uptime: ${hours}h ${minutes}m ${seconds}s`;
+
+}
+}
